1395c1be9ed2bc7b2ea6557c9e1aac1a4a4ec92e
Krzysztof Dzido <xtofek@gmail.com>
2018-05-16 18:27:48 +0200

    - sample unit test

---
+++ backend-zuul-service/src/test/groovy/com/kdzido/thesis/zuul/SampleTest.groovy
@@ -0,0 +1,14 @@
+package com.kdzido.thesis.zuul
+
+import spock.lang.Specification
+
+/**
+ * @author krzysztof.dzido@gmail.com
+ */
+class SampleSpec extends Specification {
+
+    def "should pass"() {
+        expect:
+        true
+    }
+}
